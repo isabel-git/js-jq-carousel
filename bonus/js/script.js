fdde7c4d3a7b53ca45edf1cbaf31af41d3abe1c6
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-03 21:13:58 +0100

    bonus completo e iddentazione sistemata

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    
-    $(".next").click(nextImg);
-    $(".prev").click(prevImg);
-    $(document).keydown(keyBoard);
+
+  $(".next").click(nextImg);
+  $(".prev").click(prevImg);
+  $(document).keydown(keyBoard);
 
   // funzione per l'avanti
   function nextImg() {
@@ -12,7 +12,7 @@ $(document).ready(function () {
     controlNext("img");
 
     // circle right
-    controlNext(".nav > i");  
+    controlNext(".nav > i");
   }
 
   // funzione per precedenti
@@ -26,31 +26,86 @@ $(document).ready(function () {
 
   }
 
-//   TASTIERA
+  //   TASTIERA
   function keyBoard(e) {
-    
+
     // alla pressione del tasto destro
     if (e.keyCode === 39) {
-        console.log(e.keyCode);
+      console.log(e.keyCode);
 
-        // arrow right
-        controlNext("img");
+      // arrow right
+      controlNext("img");
 
-        // circle right
-        controlNext(".nav > i"); 
-
+      // circle right
+      controlNext(".nav > i");
 
       // alla pressione del tasto sinistro  
     } else if (e.keyCode === 37) {
-        console.log(e.keyCode);
-        
-        // arrow left
-        controlPrev("img");
+      console.log(e.keyCode);
 
-        // circle left
-        controlPrev(".nav > i");
-    }    
+      // arrow left
+      controlPrev("img");
+
+      // circle left
+      controlPrev(".nav > i");
+    }
   };
+
+
+  // metto a ciascun elemento un numero univoco
+  $(function () {
+    i = 1;
+    $('.images').find('img').each(function () {
+      $(this).attr('id', i);
+      i++;
+    });
+  });
+
+  $(function () {
+    i = 5;
+    $('.nav').find('i').each(function () {
+      $(this).attr('id', i);
+      i++;
+    });
+  });
+
+
+  // circle click
+  $('.nav > i').click(function () {
+    var attivaCircle = $(".nav > i.active");
+    var attivaImg = $("img.active");
+
+    attivaCircle.removeClass("active");
+    $(this).addClass("active");
+    attivaImg.removeClass("active");
+
+    var id = $(this).attr('id');
+    console.log(id);
+
+    // click non lineare, associa ai pallini in ordine numerico le immagini
+    switch (id) {
+      case "5":
+
+        $('#1').addClass('active');
+        console.log(id);
+        break;
+
+      case "6":
+        $('#2').addClass('active');
+        console.log(id);
+        break;
+
+      case "7":
+        $('#3').addClass('active');
+        console.log(id);
+        break;
+
+      case "8":
+        $('#4').addClass('active');
+        console.log(id);
+    }
+
+  });
 
   //   funzione next
   function controlNext(element) {
@@ -59,7 +114,7 @@ $(document).ready(function () {
     attiva.removeClass("active");
 
     // quando arrivo all'ultimo elemento allora
-    if (attiva.hasClass("last")){
+    if (attiva.hasClass("last")) {
       //torna al primo
       $(element + ".first").addClass("active");
     } else {// altrimenti
@@ -68,7 +123,7 @@ $(document).ready(function () {
     }
   }
 
-    //   funzione prev
+  //   funzione prev
   function controlPrev(element) {
     var attiva = $(element + ".active");
     console.log(attiva);
@@ -76,16 +131,13 @@ $(document).ready(function () {
     attiva.removeClass("active");
 
     // quando arriva al primo elemento allora
-    if (attiva.hasClass("first")){
+    if (attiva.hasClass("first")) {
       //torna all'ultimo
       $(element + ".last").addClass("active");
     } else {// altrimenti
       //attiva quello successivo
       attiva.prev(element).addClass("active");
-    }   
+    }
   }
 
 });
-
- 
-
--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    
-    $(".next").click(nextImg);
-    $(".prev").click(prevImg);
-    $(document).keydown(keyBoard);
+
+  $(".next").click(nextImg);
+  $(".prev").click(prevImg);
+  $(document).keydown(keyBoard);
 
   // funzione per l'avanti
   function nextImg() {
@@ -22,39 +22,39 @@ $(document).ready(function () {
   }
 
   //   funzione next
-  function controlNext(element) {
-    var attiva = $(element + ".active");
+  function controlNext() {
+    var attiva = $("img.active");
 
     attiva.removeClass("active");
 
-    // quando arrivo all'ultimo elemento allora
-    if (attiva.hasClass("last")){
-      //torna al primo
-      $(element + ".first").addClass("active");
+    // quando arrivo all'ultimo img allora
+    if (attiva.hasClass("last")) {
+      //torna al primo img
+      $("img.first").addClass("active");
     } else {// altrimenti
       //attiva quello successivo
-      attiva.next(element).addClass("active");
+      attiva.next("img").addClass("active");
     }
   }
 
-    //   funzione prev
-  function controlPrev(element) {
-    var attiva = $(element + ".active");
+  //   funzione prev
+  function controlPrev() {
+    var attiva = $("img.active");
     console.log(attiva);
 
     attiva.removeClass("active");
 
-    // quando arriva al primo elemento allora
-    if (attiva.hasClass("first")){
-      //torna all'ultimo
-      $(element + ".last").addClass("active");
+    // quando arriva al primo img allora
+    if (attiva.hasClass("first")) {
+      //torna all'ultimo img
+      $("img.last").addClass("active");
     } else {// altrimenti
       //attiva quello successivo
-      attiva.prev(element).addClass("active");
-    }   
+      attiva.prev("img").addClass("active");
+    }
   }
 
 });
 
- 
+
 
