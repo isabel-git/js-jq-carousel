161395e241781d93beb7e94c24aba34366567e34
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-03 18:41:31 +0100

    aggiunto funzioni controllo

--- js/script.js
+++ js/script.js
@@ -2,15 +2,39 @@ $(document).ready(function () {
     
     $(".next").click(nextImg);
     $(".prev").click(prevImg);
-
-    var left = 37, right = 39;
-    $(document).keydown(keyPrev);
-    // $(".prev").keydown(keyNext);
-
+    $(document).keydown(keyBoard);
 
   // funzione per l'avanti
   function nextImg() {
 
+    controlNext();
+  }
+
+  // funzione per precedenti
+  function prevImg() {
+
+    controlPrev()
+
+  }
+
+  function keyBoard(e) {
+    
+    // alla pressione del tasto destro
+    if (e.keyCode === 39) {
+        console.log(e.keyCode);
+
+        controlNext();
+
+      // alla pressione del tasto sinistro  
+    } else if (e.keyCode === 37) {
+        console.log(e.keyCode);
+        
+        controlPrev()   
+    }    
+};
+
+  //   funzione next
+  function controlNext() {
     var imgAttiva = $("img.active");
 
     imgAttiva.removeClass("active");
@@ -23,12 +47,10 @@ $(document).ready(function () {
       //fai diventare attiva l'immagine successiva
       imgAttiva.next("img").addClass("active");
     }
-
   }
 
-  // funzione per precedenti
-  function prevImg() {
-
+    //   funzione prev
+  function controlPrev() {
     var imgAttiva = $("img.active");
 
     imgAttiva.removeClass("active");
@@ -40,51 +62,10 @@ $(document).ready(function () {
     } else {// altrimenti
       //fai diventare attiva l'immagine successiva
       imgAttiva.prev("img").addClass("active");
-    }
-
+    } 
   }
 
 });
 
  
-function keyPrev(e) {
 
-    // alla pressione del tasto sinistro
-    if (e.keyCode === 37) {
-        console.log(e.keyCode);
-        
-
-        var imgAttiva = $("img.active");
-
-        imgAttiva.removeClass("active");
-    
-        // se dove sono è la prima img allora
-        if (imgAttiva.hasClass("first")){
-          //torna all'ultima img
-          $("img.last").addClass("active");
-        } else {// altrimenti
-          //fai diventare attiva l'immagine successiva
-          imgAttiva.prev("img").addClass("active");
-        }    
-        
-    
-
-    // alla pressione del tasto destro
-    }else if (e.keyCode === 39) {
-        console.log(e.keyCode);
-
-        var imgAttiva = $("img.active");
-
-        imgAttiva.removeClass("active");
-    
-        // se dove sono è l'ultima img allora
-        if (imgAttiva.hasClass("last")){
-          //torna alla prima img
-          $("img.first").addClass("active");
-        } else {// altrimenti
-          //fai diventare attiva l'immagine successiva
-          imgAttiva.next("img").addClass("active");
-        }
-
-    }
-  };
